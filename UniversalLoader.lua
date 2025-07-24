ed389ad2ed9c23060bd9b83be457e3120e87bf43
Seisen88 <zandrixjanpongos46@gmail.com>
2025-07-25 00:39:56 +0800

    Update UniversalLoader.lua

--- UniversalLoader.lua
+++ UniversalLoader.lua
@@ -1,1 +1,16 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/refs/heads/main/GameList.lua"))()
+local listUrl = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/refs/heads/main/GameList.lua"
+
+local function safeHttpGet(url)
+    local ok, res = pcall(game.HttpGet, game, url)
+    return ok and res or nil
+end
+
+local data = safeHttpGet(listUrl)
+if data then
+    local games = loadstring("return " .. data)()
+    local id = tostring(game.PlaceId)
+    local scriptUrl = games[id]
+    if scriptUrl then
+        loadstring(safeHttpGet(scriptUrl))()
+    end
+end
