ba7d64c9285c391bf3cec5d87ef4c689f3ec0510
Seisen88 <zandrixjanpongos46@gmail.com>
2025-07-25 00:32:56 +0800

    Update UniversalLoader.lua

--- UniversalLoader.lua
+++ UniversalLoader.lua
@@ -1,3 +1,1 @@
-return {
-    ["11523257493"] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/refs/heads/main/Swordburst%203"
-}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/refs/heads/main/GameList.lua"))()
