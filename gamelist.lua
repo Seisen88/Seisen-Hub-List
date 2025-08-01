fb460e6b33775e0c585555dd6d1e4159cf930da7
Seisen88 <zandrixjanpongos46@gmail.com>
2025-08-01 14:39:17 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -3,6 +3,8 @@ local Games = {
      ["4093155512"] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Swordburst3.lua",
     -- Dungeon Heroes
     ["7546582051"] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Dungeonheroes.lua",
+    -- Anime Eternal
+    ["788282945"} = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/refs/heads/main/AnimeEternal.lua",
 }
 
 return Games
