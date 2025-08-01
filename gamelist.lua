fcb4919033c1944bb63bafd47e5d78fcd60d7854
Seisen88 <zandrixjanpongos46@gmail.com>
2025-08-01 14:15:02 +0800

    Update gamelist.lua

--- gamelist.lua
+++ gamelist.lua
@@ -1,5 +1,10 @@
 local Games = {
-    [11523257493] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Swordburst3.lua",
+    -- Swordburst 3 floors
+    [1481949038] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Swordburst3.lua", -- Floor 1
+    [11523258881] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Swordburst3.lua", -- Floor 2
+    [11523259322] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Swordburst3.lua", -- Floor 3
+
+    -- Other games
     [94845773826960] = "https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/Dungeonheroes.lua",
 }
 
