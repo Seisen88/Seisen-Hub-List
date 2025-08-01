2dbf936bf64b22cff34c6dd16285a025378c1a49
Seisen88 <zandrixjanpongos46@gmail.com>
2025-08-01 14:24:40 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,4 @@
 local gamelist_content = game:HttpGet("https://raw.githubusercontent.com/Seisen88/Seisen-Hub-List/main/gamelist.lua")
-
 if not gamelist_content then return end
 
 local success, Games = pcall(function()
@@ -9,10 +8,12 @@ end)
 if not success or type(Games) ~= "table" then return end
 
 local gameId = tostring(game.GameId)
-local scriptURL = Games[gameId]
+local placeId = tostring(game.PlaceId)
+
+local scriptURL = Games[gameId] or Games[placeId] -- check GameId first, then fallback to PlaceId
 
 if scriptURL then
     loadstring(game:HttpGet(scriptURL))()
 else
-    warn("No supported script found for this game. GameId: " .. gameId)
+    warn("No supported script found for this game. GameId: " .. gameId .. ", PlaceId: " .. placeId)
 end
